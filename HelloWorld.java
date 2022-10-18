79a507c0126f2724646e0e9707a89f3b64e7b673
vishymails <vishymails@gmail.com>
2022-10-18 15:37:02 +0530

    hello program added

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String [] s) {
+        System.out.println("Hello All");
+        for(int i = 0; i < 20; i ++) {
+            System.out.println("Value Of I : " + i);
+        }
+    }
+}
