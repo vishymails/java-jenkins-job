f1d1b0331086aa5153215c17811d18363b57ecb7
vishymails <vishymails@gmail.com>
2023-01-17 16:53:08 +0530

    java file added

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] s) {
+        System.out.println("Hello Oracle");
+        for(int i=0; i<=10; i++) {
+            System.out.println(i);
+        }
+    }
+}
